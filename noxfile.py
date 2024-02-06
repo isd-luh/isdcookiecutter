357c32321c33ac7ee9066c6ce4ea6314f3d69f61
Stefan Warnken <s.warnken@isd.uni-hannover.de>
2024-02-06 15:02:48 +0100

    add pyproject.toml and noxfile

---
+++ noxfile.py
@@ -0,0 +1,22 @@
+import nox
+
+
+@nox.session
+def lint(session):
+    """Lint all python files with ruff.
+    see pyproject.toml for configuration
+    fix unsorted imports
+    """
+    session.install("ruff")
+    session.run("ruff", ".", "--fix")
+    session.run("ruff", "format", ".")
+
+
+@nox.session
+def docs(session):
+    """Create documentation with mkdocs and mkdocstrings.
+    see mkdocs.yml and docs/index.md
+    """
+    session.install("-r", "docs/requirements.txt")
+    session.install("./")
+    session.run("mkdocs", "build", "-d", "public")
